88fbba1aee8f2d9699abc8b3ba2f9e7627176f98
ranjan <ranjanrnayak2004@gmail.com>
2022-02-27 16:33:26 +0530

    Initial stable commit

---
+++ healthcare/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <p className='title'>
+          HealthCare
+        </p>
+
+
+        <hr class="solid"></hr>
+
+        
+        <img src="./image.png" width="500" height="200" />
+
+        <br></br>
+        <br></br>
+        <br></br>
+  
+    <button className='btn'>Scan Any Ingredient</button>
+
+  </div>
+  )
+}
+export default App 
